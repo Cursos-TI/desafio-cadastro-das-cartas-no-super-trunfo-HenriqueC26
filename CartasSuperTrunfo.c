238e35d52db40ecaa220b74043c176d9e2d6d1cd
HenriqueC26 <rikecarvalho@hotmail.com>
2025-02-25 14:12:14 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,35 +8,53 @@ int main() {
     float area1, area2, pib1, pib2;
     int pontos1, pontos2;
 
+    // Dados da primeira carta
+    
     printf("Digite os dados da primeira carta:\n");
+    
     printf("Estado (A-H): ");
     scanf(" %c", &estado1);
+    
     printf("Código da carta: ");
     scanf("%s", &codigo1);
+    
     printf("Nome da Cidade: ");
     scanf(" %[^\n]", nomeCidade1);
+    
     printf("População: ");
     scanf("%d", &populacao1);
+    
     printf("Área (em km²): ");
     scanf("%f", &area1);
+    
     printf("PIB (em bilhões de reais): ");
     scanf("%f", &pib1);
+    
     printf("Número de pontos Turísticos: ");
     scanf("%d", &pontos1);
 
+    // Dados da segunda carta
+    
     printf("Digite os dados da segunda carta:\n");
+    
     printf("Estado (A-H): ");
     scanf(" %c", &estado2);
+    
     printf("Código da carta: ");
     scanf("%s", &codigo2);
+    
     printf("Nome da Cidade: ");
     scanf(" %[^\n]", nomeCidade2);
+    
     printf("População: ");
     scanf("%d", &populacao2);
+    
     printf("Área (em km²): ");
     scanf("%f", &area2);
+    
     printf("PIB (em bilhões de reais): ");
     scanf("%f", &pib2);
+    
     printf("Número de pontos Turísticos: ");
     scanf("%d", &pontos2);
 }
